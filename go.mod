27aa38e56d6ed6a288e60decd24c16dc1450a25f
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2023-01-16 18:56:38 -0800

    Moving over translation patch

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/beauxarts/divido
 
 go 1.19
+
+require golang.org/x/exp v0.0.0-20230116083435-1de6713980de // indirect
---
+++ translation_patch.go
@@ -0,0 +1,92 @@
+package divido
+
+import (
+	"golang.org/x/exp/maps"
+	"io"
+	"sort"
+	"strings"
+)
+
+type xhtmlDecorations struct {
+	prefix, suffix int
+}
+
+type translationPatch struct {
+	sourceLines        []string
+	contentDecorations map[int]xhtmlDecorations
+	translatedContent  []string
+}
+
+func NewTranslationPatch(lines ...string) *translationPatch {
+	return &translationPatch{
+		sourceLines: lines,
+	}
+}
+
+func (tp *translationPatch) UpdateContentDecorations() {
+
+	tp.contentDecorations = make(map[int]xhtmlDecorations)
+
+	for li, line := range tp.sourceLines {
+		prefix, suffix := xhtmlDecorationsForLine(line)
+		if prefix == -1 {
+			continue
+		}
+		tp.contentDecorations[li] = xhtmlDecorations{prefix: prefix, suffix: suffix}
+	}
+}
+
+func xhtmlDecorationsForLine(line string) (int, int) {
+	prefix := strings.Index(line, ">")
+	if prefix != -1 && prefix < len(line) {
+		prefix += 1
+	}
+	suffix := strings.LastIndex(line, "<")
+	if prefix > suffix {
+		prefix = -1
+	}
+	return prefix, suffix
+}
+
+func (tp *translationPatch) SourceContent() []string {
+
+	content := make([]string, 0, len(tp.contentDecorations))
+
+	order := maps.Keys(tp.contentDecorations)
+	sort.Ints(order)
+
+	for _, li := range order {
+		xd := tp.contentDecorations[li]
+		if xd.prefix == -1 {
+			continue
+		}
+		content = append(content, tp.sourceLines[li][xd.prefix:xd.suffix])
+	}
+
+	return content
+}
+
+func (tp *translationPatch) AddTranslatedContent(tc []string) {
+	tp.translatedContent = append(tp.translatedContent, tc...)
+}
+
+func (tp *translationPatch) Apply(w io.Writer) error {
+
+	index := 0
+	for li, line := range tp.sourceLines {
+		if xd, ok := tp.contentDecorations[li]; ok {
+			// redecorate
+			translatedLine := tp.translatedContent[index]
+
+			line = line[:xd.prefix] + translatedLine + line[xd.suffix:]
+
+			index++
+		}
+
+		if _, err := io.WriteString(w, line+"\n"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
